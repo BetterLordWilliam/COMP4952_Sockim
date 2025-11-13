e821375d5283fb2b9a24d810abe5d078afecd017
BetterLordWilliam <willjordan05@icloud.com>
2025-11-12 20:16:37 -0800

    WO: IMPORTANT COMMIT, THE APP IS IN A MINIMAL WORKING CONDITION. MAKING NEW BRANCH FOR USER IN CHAT CRUD.

--- Models/ChatDto.cs
+++ Models/ChatDto.cs
@@ -2,11 +2,16 @@ using System;
 
 namespace COMP4952_Sockim.Models;
 
-public class ChatDto
+public class ChatDto : IEquatable<ChatDto>
 {
     public int Id { get; set; }
     public string ChatName { get; set; } = string.Empty;
     public int ChatOwnerId { get; set; }
     public string ChatOwnerEmail { get; set; } = string.Empty;
     public List<string> InvitedEmails { get; set; } = [];
+
+    public bool Equals(ChatDto? other)
+    {
+        return Id == other!.Id;
+    }
 }
--- Models/ChatUserDto.cs
+++ Models/ChatUserDto.cs
@@ -2,8 +2,13 @@ using System;
 
 namespace COMP4952_Sockim.Models;
 
-public class ChatUserDto
+public class ChatUserDto : IEquatable<ChatUserDto>
 {
     public int Id { get; set; }
     public string Email { get; set; } = string.Empty;
+
+    public bool Equals(ChatUserDto? other)
+    {
+        return Id == other!.Id;
+    }
 }
